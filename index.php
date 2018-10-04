52df47c9b1999980afbd1422b0690c7d2a782c6e
Webmaster <webmaster@weblogica.mx>
2018-10-04 11:04:32 -0500

    dominios

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include './componentes/header/header.php'; ?>
 <main id="content"> 
   <aside id="aside"><?php include './componentes/aside/aside.php';?></aside>
-  <div class=" pagina active" id="planes-de-hospedaje-web" ><?php   include './paginas/planes-de-hospedaje-web/planes-de-hospedaje-web.html'; ?></div>
+  <div class=" pagina " id="planes-de-hospedaje-web" ><?php   include './paginas/planes-de-hospedaje-web/planes-de-hospedaje-web.html'; ?></div>
   <div class=" pagina " id="dominios"><?php include './paginas/dominios/dominios.html';?></div>
   <div class=" pagina " id="promociones"><?php include './paginas/promociones/promociones.html';?></div>
   <div class=" pagina " id="home" ><?php include './paginas/home/home.html';?></div>
