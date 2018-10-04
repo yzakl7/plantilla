778edc1091507562be3388f3707ea1e9ba873926
Webmaster <webmaster@weblogica.mx>
2018-10-03 19:25:48 -0500

    glosario (faltan terminos)

--- paginas/planes-de-hospedaje-web/glosario-de-terminos/glosario-de-terminos.js
+++ paginas/planes-de-hospedaje-web/glosario-de-terminos/glosario-de-terminos.js
@@ -1,1 +1,12 @@
-window.onload = alert(' gklosario de terminos.js')
+function filtrarGlosario(letra){
+    var glosarios = document.getElementsByClassName('glosario');
+    for(var i= 0; i < glosarios.length; i++ ){
+        glosarios[i].classList.add('oculto');
+    }
+    console.log(letra);
+    console.log('glosario-'+letra);
+    console.log(document.getElementById('letra'));
+    console.log(document.getElementById('glosario-'+letra));
+    document.getElementById('letra').innerHTML = letra;
+    document.getElementById('glosario-'+letra).classList.remove('oculto')
+}
