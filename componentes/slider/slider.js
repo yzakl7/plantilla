abac4e168a0b7757e625cae356bb6e7bdde95703
yzakl7 <isaacsauriortegon@gmail.com>
2018-10-08 02:31:02 -0500

    slider dinamico, falta touch y autoloop

--- componentes/slider/slider.js
+++ componentes/slider/slider.js
@@ -1,25 +1,90 @@
-var diapositiva = document.createElement('div');
+var slider = document.getElementById('wl-slider');
 
+var diapositivas = [
+    {
+        titulo: 'HOLA! INTERNET!',
+        descripcion: 'Fusionando la tradición con la modernidad, Galletas Dondé utiliza las tendencias actuales en tecnología para fortalecer vínculos con sus consumidores.',
+        link: '',
+        img: './componentes/slider/img/donde.jpg',
+        alt:'Dondé'
+    },
+    {
+        titulo: 'HOLO! INTERNET!',
+        descripcion: 'Fusionando la tradición con la modernidad, Galletas Dondé utiliza las tendencias actuales en tecnología para fortalecer vínculos con sus consumidores.',
+        link: '',
+        img: './componentes/slider/img/donde.jpg',
+        alt:'Dondé'
+    },
+    {
+        titulo: 'HOLA! INTERNET!',
+        descripcion: 'Fusionando la tradición con la modernidad, Galletas Dondé utiliza las tendencias actuales en tecnología para fortalecer vínculos con sus consumidores.',
+        link: '',
+        img: './componentes/slider/img/donde.jpg',
+        alt:'Dondé'
+    },
+    {
+        titulo: 'HOLA! INTERNET!',
+        descripcion: 'Fusionando la tradición con la modernidad, Galletas Dondé utiliza las tendencias actuales en tecnología para fortalecer vínculos con sus consumidores.',
+        link: '',
+        img: './componentes/slider/img/donde.jpg',
+        alt:'Dondé'
+    },
+]
 
-var docu = document.getElementById('wl-slider')
-var texto = 'werwer'
-
-
-var snippet =`
-<div class="text-container">
-            <h3>${texto}</h3>
-            <p>
-                Fusionando la tradición con la modernidad,
-                Galletas Dondé utiliza las tendencias actuales en tecnología
-                para fortalecer vínculos con sus consumidores.
-            </p>
-            <a href="">Conoce más</a>
-        </div>
-        <div class="img-container">
-            <img src="./componentes/slider/img/donde.jpg" alt="">
-            <div id="indicators">
-                <div onClick="sirve()" class="indicator"></div>
+for(var i = 0; i < diapositivas.length; i++){
+    var 
+        indicators = document.getElementById('indicators'),
+        slide = document.createElement('div'),
+        indicator = document.createElement('div'),
+        snippet =`
+            <div class="text-container">
+                <h3>${diapositivas[i].titulo}</h3>
+                <p>
+                    ${diapositivas[i].descripcion}
+                </p>
+                <a href="${diapositivas[i].link}">Conoce más</a>
             </div>
-        </div>`
+            <div class="img-container">
+                <img src="${diapositivas[i].img}" alt="${diapositivas[i].alt}">
+            </div>
+        `
+    indicator.setAttribute('onClick' ,'showSlide('+i+')')
+    indicator.classList = 'indicator'
+    
+    indicators.appendChild(indicator)
 
-docu.innerHTML = snippet
+
+    slide.classList.add('diapositiva');
+    if(i!=0){
+        slide.classList.add('hidden');
+    }
+    slide.setAttribute("id", "diapositiva-"+i);
+    // slide.id('diapositiva-'+i);
+    slide.innerHTML = snippet;
+
+    slider.appendChild(slide);
+}
+
+var current, previous
+
+function showSlide(n){
+    current = n
+    if(typeof previous != 'number'){
+        previous = 0
+    }
+
+    if(current != previous){
+        if(current > previous){
+            document.getElementById('diapositiva-'+previous).classList = 'diapositiva toRight'
+            document.getElementById('diapositiva-'+current).classList = "diapositiva fromLeft"
+        }else{
+            document.getElementById('diapositiva-'+previous).classList = 'diapositiva toLeft'
+            document.getElementById('diapositiva-'+current).classList = "diapositiva fromRight"
+        }
+    }
+    
+    previous = n
+    
+}
+
+u
